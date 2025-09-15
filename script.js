e9392477178490883d22aad00a28984724560a75
Mariana Nogueira <161905519+marinogne@users.noreply.github.com>
2025-09-14 21:09:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-
-    botaoDeAcessibilidade.addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade-batata');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade-batata');
+ 
+    botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-
-    })
-
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+ 
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+    });
+ 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte-batata');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte-batata');
+ 
 
     let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
+ 
+    aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+ 
+    diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-})
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+ 
+   
+});
+ 
+ScrollReveal().reveal('#inicio-batata', { delay: 500 });
+ScrollReveal().reveal('#historia-batata', { delay: 500 });
+ScrollReveal().reveal('#galeria-batata', { delay: 500 });
+ScrollReveal().reveal('#contato-batata', { delay: 500 });
